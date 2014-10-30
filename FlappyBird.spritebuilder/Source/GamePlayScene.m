980f95967a391970b922dee5c71407019879574b
Brittany D <WWJD@Mynames-MacBook-Pro.local>
2014-10-29 17:51:27 -0700

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,12 +10,27 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
     
     
     
 }
 
 -(void)update:(CCTime)delta
+// this will be run every frame
+// delta is the time that has elapsed since the last time it was run.
+// This is usually 1/60th of a second, but can be bigger if the game slows down.
+
+timeSinceObstacle += delta; //approximately 1/60th of a second
+// check to see if 2 seconds have passed
+if (timeSinceObstacle > 2.0f)
+{
+    // add a new obstacle
+    [self addObstacle];
+    
+    //then reset the timer
+    timeSinceObstacle = 0.0f
+}
 {
     // put update code here
 }
